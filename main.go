b05e87780a726c342ddb9def003b83fe11d279e9
Martin Schütte <martin.schuette@deck36.de>
2014-06-27 13:09:37 +0200

    really use the verbose and nofollow flags

--- main.go
+++ main.go
@@ -37,8 +37,8 @@ type CommandLineOptions struct {
 	exchangeName *string
 	exchangeType *string
 	routingKey   *string
-	verbose      bool
-	nofollow     bool
+	verbose      *bool
+	nofollow     *bool
 }
 
 var options CommandLineOptions
@@ -52,8 +52,8 @@ func init() {
 		flag.String("exchange", "logtest", "Durable AMQP exchange name"),
 		flag.String("exchange-type", "fanout", "Exchange type - direct|fanout|topic|x-custom"),
 		flag.String("key", "nginxlog", "AMQP routing key"),
-		*flag.Bool("v", false, "Verbose output"),
-		*flag.Bool("n", false, "Quit after file is read, do not wait for more data, do not read/write state"),
+		flag.Bool("v", false, "Verbose output"),
+		flag.Bool("n", false, "Quit after file is read, do not wait for more data, do not read/write state"),
 	}
 	flag.Parse()
 }
@@ -111,7 +111,13 @@ func readLogsFromFile(fname string, queue chan<- Logline, shutdown chan<- string
 	var statefile string
 	var offset int64
 	var inode uint64
-	if !options.nofollow {
+	var doFollowFile bool = !*options.nofollow
+
+	if *options.verbose {
+		log.Printf("readLogsFromFile: dofollow=%v", doFollowFile)
+	}
+
+	if doFollowFile {
 		statefile = fname + ".state"
 		inode     = readFileInode(fname)
 		offset    = readStateFile(fname, statefile, inode)
@@ -119,8 +125,8 @@ func readLogsFromFile(fname string, queue chan<- Logline, shutdown chan<- string
 
 	// setup
 	config := tail.Config{
-		Follow:    true,
-		ReOpen:    true,
+		Follow:    doFollowFile,
+		ReOpen:    doFollowFile,
 		MustExist: true,
 		Logger:    tail.DiscardingLogger,
 		Location: &tail.SeekInfo{
@@ -131,20 +137,27 @@ func readLogsFromFile(fname string, queue chan<- Logline, shutdown chan<- string
 	t, err := tail.TailFile(fname, config)
 	if err != nil {
 		shutdown <- fmt.Sprintf("cannot tail file %s: %v", fname, err)
+	} else if *options.verbose {
+		log.Printf("opened log file %s", fname)
 	}
 
 	// now just sleep and wait for input and control channel
 	for {
 		select {
 		case line := <-t.Lines:
-			queue <- Logline(line.Text)
+			if line != nil {
+				queue <- Logline(line.Text)
+			} else {
+				shutdown <- "Logfile closed"
+				return
+			}
 		case <-savestate:
 			offset, _ := t.Tell()
-			if options.nofollow {
+			if doFollowFile {
 				writeStateFile(statefile, inode, offset)
 			}
-			if options.verbose {
-				log.Println("reading %s, now at offset %d", fname, offset)
+			if *options.verbose {
+				log.Printf("reading %s, now at offset %d", fname, offset)
 			}
 		}
 	}
@@ -153,6 +166,9 @@ func readLogsFromFile(fname string, queue chan<- Logline, shutdown chan<- string
 // open AMQP channel
 func openAmqpChannel(amqpURI string, exchange string, exchangeType string, routingKey string) (connection *amqp.Connection, channel *amqp.Channel, err error) {
 	// this is the important part:
+	if *options.verbose {
+		log.Println("connecting to ", amqpURI, "...")
+	}
 	connection, err = amqp.Dial(amqpURI)
 	if err != nil {
 		return nil, nil, fmt.Errorf("AMQP Dial: %s", err)
@@ -191,8 +207,8 @@ func writeLogsToAmqp(queue <-chan Logline, shutdown chan<- string) {
 	connection, channel, err := openAmqpChannel(*options.uri,
 		*options.exchangeName, *options.exchangeType, *options.routingKey)
 	failOnError(err, "cannot open AMQP channel")
-	if options.verbose {
-		log.Println("opened AMQP connection")
+	if *options.verbose {
+		log.Println("opened AMQP connection and checked exchange")
 	}
 	defer connection.Close()
 	defer channel.Close()
@@ -207,7 +223,7 @@ func writeLogsToAmqp(queue <-chan Logline, shutdown chan<- string) {
 		err := publishSingleMessageToAmqp(message, channel)
 		if err != nil {
 			failOnError(err, "AMQP error")
-		} else if options.verbose {
+		} else if *options.verbose {
 			fmt.Printf(".")
 		}
 	}
@@ -245,23 +261,29 @@ func osSignalHandler(shutdown chan<- string) {
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	sig := <-sigs
+
 	shutdown <- fmt.Sprintf("received signal %v", sig)
 }
 
 func main() {
+	if *options.verbose {
+		log.Printf("Start, verbose: %v, nofollow: %v", *options.verbose, *options.nofollow)
+	}
 	// let goroutines tell us to shutdown (on error)
-	var shutdown = make(chan string)
+	var sig_shutdown  = make(chan string)
+	var file_shutdown = make(chan string)
+	var amqp_shutdown = make(chan string)
 	// the main data queue, between reader and writer goroutines
 	var queue = make(chan Logline)
 
 	// let the OS tell us to shutdown
-	go osSignalHandler(shutdown)
+	go osSignalHandler(sig_shutdown)
 
 	// tell goroutine to save state before shutdown
 	var savestate = make(chan bool)
-	go readLogsFromFile(*options.filename, queue, shutdown, savestate)
+	go readLogsFromFile(*options.filename, queue, file_shutdown, savestate)
 
-	go writeLogsToAmqp(queue, shutdown)
+	go writeLogsToAmqp(queue, amqp_shutdown)
 
 	// keep track of last offset
 	ticker := time.NewTicker(time.Second * 2)
@@ -271,7 +293,22 @@ func main() {
 		}
 	}()
 
-	message := <-shutdown
-	savestate <- true
-	log.Println("The End.", message)
+	select {
+	case message := <-sig_shutdown:
+		if *options.verbose {
+			log.Println("sig_shutdown:", message)
+		}
+	case message := <-file_shutdown:
+		if *options.verbose {
+			log.Println("file_shutdown:", message)
+		}
+	case message := <-amqp_shutdown:
+		if *options.verbose {
+			log.Println("amqp_shutdown:", message)
+		}
+		savestate <- true  // file reader still alive
+	}
+	if *options.verbose {
+		log.Println("The End.")
+	}
 }
